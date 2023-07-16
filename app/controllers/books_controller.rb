6abb2962a37e843b25ce1f1e5e1febc7b0832b54
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-07-16 17:53:14 +0000

    [add] newフォーム

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,42 @@
+class BooksController < ApplicationController
+
+  def new
+    @new_book = Book.new
+  end
+
+  def create
+    @new_book = Book.new(book_params)
+    @new_book.user_id = current_user.id
+
+    if @new_book.save
+      redirect_to book_path(@new_book.id)
+    else
+      render :new
+    end
+
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def edit
+    @edit_book = Book.find(params[:id])
+  end
+  
+  def update
+    @edit_book = Book.find(params[:id])
+    @edit_book.user_id = current_user.id
+    
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   
   devise_for :users
   
+  resources :users, only: [:show, :edit]
+  resources :books, only: [:index, :show, :edit, :create]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
